c6867a194c6d8158caf528e3d58f6485ed3ff1f0
granitexe <37618762+granitexe@users.noreply.github.com>
2023-05-22 11:07:11 +0200

    Update GANSeg.py

--- GANSeg.py
+++ GANSeg.py
@@ -274,17 +274,20 @@ class GANSeg(object) :
                 if not self.no_gan:
                     self.D_optim.param_groups[0]['lr'] -= (self.lr / (self.iteration // 2))
 
-            try:
-                real_A, real_seg_A = trainA_iter.next()
-            except:
-                trainA_iter = iter(self.trainA_loader)
-                real_A, real_seg_A = trainA_iter.next()
+            trainA_iter = iter(self.trainA_loader)
+            trainB_iter = iter(self.trainB_loader)
 
             try:
-                real_B, _ = trainB_iter.next()
-            except:
+                real_A, real_seg_A = next(trainA_iter)
+            except StopIteration:
+                trainA_iter = iter(self.trainA_loader)
+                real_A, real_seg_A = next(trainA_iter)
+
+            try:
+                real_B, _ = next(trainB_iter)
+            except StopIteration:
                 trainB_iter = iter(self.trainB_loader)
-                real_B, _ = trainB_iter.next()
+                real_B, _ = next(trainB_iter)
 
             real_A, real_B = real_A.to(self.device, dtype=torch.float), real_B.to(self.device, dtype=torch.float)
             real_seg_A = real_seg_A.to(self.device, dtype=torch.long)
